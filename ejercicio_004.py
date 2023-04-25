4b54ca638c3c829c0affe94b2ec7057110e2b714
lucybrit <131091129+lucybrit@users.noreply.github.com>
2023-04-25 11:08:59 -0600

    Add files via upload

---
+++ ejercicio_004.py
@@ -0,0 +1,7 @@
+#Tupla
+
+a= (1,2,3,4,5,6)
+b = (-1,0,0,1,1,1)
+
+print (f"La {type(a)} con una longitud de {len(a)} y los siguientes valores {a} ")
+print (f"La {type(b)} con una longitud de {len(b)} y los siguientes valores {b} ")
