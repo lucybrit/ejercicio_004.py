10566b057e6f0789d9b1aa16e81efcb636b5eb9e
lucybrit <131091129+lucybrit@users.noreply.github.com>
2023-05-04 18:52:48 -0600

    Add files via upload

--- ejercicio_004.py
+++ ejercicio_004.py
@@ -1,11 +1,21 @@
 #Tupla
-tupla =1
-a= (1,2,3,4,5,6)
-b = (-1,0,0,1,1,1)
-
-for x in a:
-   for y in b:
-      print (x+y)
+a = ((1, 2, 3),
+     (4, 5, 6))
+b = ((-1, 0),
+     (0, 1),
+     (1,1))
+result = [[0,0],
+          [0,0]]
+for i in range(len(a)):
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            result[i][j] += a[i][k] * b[k][j]
+for i in range(len(result)):
+    result[i] = tuple(result[i])
+result = tuple(result)
+for i in range(len(result)):
+    print(f"El resultado del producto es: {result[i]}")
+      
    
     
        
